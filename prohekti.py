80e1abf4b21badf99213ec15b3ea97ba6d58bb01
eRosz <cubicals@outlook.com>
2018-06-07 10:22:55 +0300

    new stuff

--- prohekti.py
+++ prohekti.py
@@ -13,34 +13,37 @@ def otaNimi(raakaTeksti):
 def otaMaara(raakaTeksti):
     return raakaTeksti[getIndex(raakaTeksti)+1:]
 
+def muutaMaara(raakaTeksti, uusi):
+    return raakaTeksti[:getIndex(raakaTeksti)] + ": " + str(uusi) + "\n"
+
 # lyukee tiedoston läpi ja etsii halutun henkilön. jos ei löydy lisää henkilön listaan. 
 # käy tiedoston läpi ja katsoo onko siellä tiettyä nimeä ja kertoo sen r0ahamäärän#
 #  kirjoittaa kokonaan tiedoston uudestaan 
 # lukea riviltä
 # ottaa vastaan nimen ja palauttaa 
 
-def nimihaku(nimi):
-    nimilöyty = False
-    avaus = open('C:/Users/OMISTAJA/Documents/pikkupankki/testii.txt','r')
-    for i in avaus.readlines():
-        if otaNimi(i) == nimi:
-            nimilöyty = True
-            print(i)
-    avaus.close()
-    if nimilöyty == False:
-        appendFile = open('C:/Users/OMISTAJA/Documents/pikkupankki/testii.txt','a')
-        appendFile.write("\n"+nimi+":0")
-        appendFile.close()
+# def nimihaku(nimi):
+#     nimilöyty = False
+#     avaus = open('D:/Lisäohjelmat/pythno/Lib/site-packages/testii.txt','r')
+#     for i in avaus.readlines():
+#         if otaNimi(i) == nimi:
+#             nimilöyty = True
+#             print(i)
+#     avaus.close()
+#     if nimilöyty == False:
+#         appendFile = open('D:/Lisäohjelmat/pythno/Lib/site-packages/testii.txt','a')
+#         appendFile.write("\n"+nimi+":0")
+#         appendFile.close()
 # nimihaku("antti")
 
-def nimiJaArvo(nimi):
-    nimilöyty1 = False
-    avaus1 = open('C:/Users/OMISTAJA/Documents/pikkupankki/testii.txt','r')
-    for i in avaus1.readlines():
-        if otaNimi(i) == nimi:
-            nimilöyty1 = True
-            return int(otaMaara(i))
-    avaus1.close()
+# def nimiJaArvo(nimi):
+#     nimilöyty1 = False
+#     avaus1 = open('D:/Lisäohjelmat/pythno/Lib/site-packages/testii.txt','r')
+#     for i in avaus1.readlines():
+#         if otaNimi(i) == nimi:
+#             nimilöyty1 = True
+#             print(otaMaara(i))
+#     avaus1.close()
 
 # nimiJaArvo('Matias')
 
@@ -54,13 +57,33 @@ def nimiJaArvo(nimi):
 
 # tekst = 'arvo'
 
-def nimihaku2(nimi):
-    nimilöyty2 = False
-    avaus2 = open('C:/Users/OMISTAJA/Documents/pikkupankki/testii.txt','r')
-    for i in avaus2.readlines():
+# def nimihaku2(nimi):
+#     nimilöyty2 = False
+#     avaus2 = open('D:/Lisäohjelmat/pythno/Lib/site-packages/testii.txt','r')
+#     for i in avaus2.readlines():
+#         if otaNimi(i) == nimi:
+#             nimilöyty2 = True
+#             print(i)
+#     avaus2.close()
+#     if nimilöyty2 == True:
+#         print('jee')
+
+def listavastaanotto(arvo):
+    tekstimuoto = ""
+    for i in arvo:
+        tekstimuoto += i
+    kirjotus = open('C:/Users/mala0/Desktop/kk2018/pikkupankki/testii.txt','w')
+    kirjotus.write(tekstimuoto)
+    kirjotus.close()
+
+def nimiarvovast(arvo, nimi):
+    tekstiLista = ['']
+    lue = open('C:/Users/mala0/Desktop/kk2018/pikkupankki/testii.txt','r')
+    for i in lue.readlines():
         if otaNimi(i) == nimi:
-            nimilöyty2 = True
-            print(i)
-    avaus2.close()
-    if nimilöyty2 == True:
-        print('ee')
+            i = muutaMaara(i, arvo)
+        tekstiLista.append(i)
+    lue.close()
+    listavastaanotto(tekstiLista)
+nimiarvovast(100, "Samuel")
+
